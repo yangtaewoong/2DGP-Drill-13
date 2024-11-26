924908433f7a1a307c2f6a4745d66a5ab989b1c6
yangtaewoong <tom0323@naver.com>
2024-11-26 13:57:51 +0900

    end

---
+++ ball.py
@@ -0,0 +1,30 @@
+from pico2d import *
+import game_world
+import game_framework
+import random
+import server
+
+class Ball:
+    image = None
+
+    def __init__(self, x=None, y=None):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x = x if x else random.randint(10, 1600)
+        self.y = y if y else random.randint(10, 780)
+
+    def draw(self):
+        sx = self.x - server.background.window_left
+        sy = self.y - server.background.window_bottom
+        self.image.draw(sx, sy)
+
+    def update(self):
+        pass
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def handle_collision(self, group, other):
+        match group:
+            case 'boy:ball':
+                game_world.remove_object(self)
